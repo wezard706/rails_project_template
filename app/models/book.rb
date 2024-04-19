70d00f4d18aca5250c0daec2a496a9f0c67a2eed
wezard706 <wezard706@gmail.com>
2024-04-19 23:13:10 +0900

    モデルを編集

--- app/models/book.rb
+++ app/models/book.rb
@@ -1,2 +1,9 @@
 class Book < ApplicationRecord
+  belongs_to :publisher
+
+  validates :title, presence: true
+  validates :description, presence: true, length: { minimum: 10, maximum: 1000 }
+  validates :price, presence: true, numericality: { only_integer: true, greater_than_or_equal_to: 0 }
+  validates :publisher, presence: true
+  validates :published_at, presence: true
 end
