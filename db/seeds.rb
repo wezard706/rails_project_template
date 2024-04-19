cc3260b20bb6b8d8ba93af5052ba76c08fc1e472
wezard706 <wezard706@gmail.com>
2024-04-19 23:13:28 +0900

    publisherのシードを追加

--- db/seeds.rb
+++ db/seeds.rb
@@ -7,3 +7,6 @@
 #   ["Action", "Comedy", "Drama", "Horror"].each do |genre_name|
 #     MovieGenre.find_or_create_by!(name: genre_name)
 #   end
+["集英社", "講談社", "KADOKAWA"].each do |name|
+  Publisher.find_or_create_by!(name:)
+end
